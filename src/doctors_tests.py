83976489b157b41af99a62e8309143b554cb433a
Johnnie Lo <johnnielo50@gmail.com>
2016-10-17 02:01:24 -0700

    Doctors Testsuite: Added test for __str__.

--- src/doctors_tests.py
+++ src/doctors_tests.py
@@ -18,5 +18,9 @@ class TestDoctorMethods(unittest.TestCase):
 		# Nothing in common
 		self.assertEqual(doctor_one.similarity_score(doctor_three), 0)
 
+	def test_str(self):
+		doctor = Doctor('3', 'Dr.Lynn', 'Dentist', 'Mountain View', 0)
+		self.assertEqual( str(doctor), 'ID: 3 Name: Dr.Lynn Specialty: Dentist Area: Mountain View Score: 0')
+
 if __name__ == '__main__':
     unittest.main()
